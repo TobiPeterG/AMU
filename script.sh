1e0097ef20381ebe6be93995bd23b4ce67afaccd
Tobi_Peter <19935382+TobiPeterG@users.noreply.github.com>
2021-10-18 14:55:42 +0200

    Add chance to stop script before execution

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-echo "This script will install my custom script to\nupdate your system when it's being shutdown.\nIt is tested on Kubuntu 21.04 and 21.10, but should work\nwith future versions as well.\n\nThe script requires root privileges to copy the files to the\ncorresponding locations. Please note that in file\nplasma-shutdown the <root-password> needs to be\nspecified in order for the script to work!\nIf you haven't done this before executing this file, you need\nto edit it and execute this script again.\n\nThis script also includes a service that updates your\nflatpak and snap packages at startup."
+echo "This script will install my custom script to\nupdate your system when it's being shutdown.\nIt is tested on Kubuntu 21.04 and 21.10, but should work\nwith future versions as well.\n\nThe script requires root privileges to copy the files to the\ncorresponding locations. Please note that in file\nplasma-shutdown the <root-password> needs to be\nspecified in order for the script to work!\nIf you haven't done this before executing this file, you need\nto edit it and execute this script again.\n\nThis script also includes a service that updates your\nflatpak and snap packages at startup.\nThis script will continue in 10 seconds.\n Press ctrl+c to abort.\n"
+sleep 10
 chmod +x ./plasma-shutdown
 chmod +x ./shutdown_update
 chmod +x ./startup_update

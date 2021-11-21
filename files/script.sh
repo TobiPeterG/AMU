cc5997b78cd47c9b47ceb33e412d5b9401ea3b4e
Tobi_Peter <19935382+TobiPeterG@users.noreply.github.com>
2021-11-21 23:55:29 +0100

    add missing comment to script.sh

--- files/script.sh
+++ files/script.sh
@@ -12,13 +12,17 @@ cp ./shutdown_update /usr/share/services/shutdown_update
 cp ./startup_update /usr/share/services/startup_update
 cp ./update_shutdown.service /etc/systemd/system/update_shutdown.service
 cp ./update_startup.service /etc/systemd/system/update_startup.service
+
+#the poweroff.target has to be modified to remove the timeout
 cp -r ./poweroff.target.d /etc/systemd/system/
+
 systemctl daemon-reload
 systemctl enable update_startup.service
 systemctl enable update_shutdown.service
 systemctl start update_shutdown.service
 chmod +x /usr/share/services/shutdown_update
 chmod +x /usr/share/services/startup_update
+
 #fix the font of plymouth not being visible of the manjaro theme
 sed -i 's/_//' /usr/share/plymouth/themes/manjaro/manjaro.plymouth
 #set the manjaro theme as default

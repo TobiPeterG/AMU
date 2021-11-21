c9bacd4992495d8c497827120a52c0563f8346f3
Tobi_Peter <19935382+TobiPeterG@users.noreply.github.com>
2021-11-21 23:53:59 +0100

    add comments to script.sh

--- files/script.sh
+++ files/script.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-echo "This script will install my custom script to\nupdate your system when it's being shutdown.\nIt is tested on Kubuntu 21.04 and 21.10, but should work\nwith future versions as well.\n\nThe script requires root privileges to copy the files to the\ncorresponding locations.\n\nThis script also includes a service that updates your\nflatpak and snap packages at startup."
+#show warning and wait for user to hit enter or abort installation
+echo "This script will install my custom script to\nupdate your system when it's being shutdown.\nIt is tested on Manjaro 21.1.6, but should work\nwith future versions as well.\n\nThe script requires root privileges to copy the files to the\ncorresponding locations.\n\nThis script also includes a service that updates your\nflatpak and snap packages at startup."
 printf 'press [ENTER] to continue, [CTRL]+[C] to abort...'
 read _
 
+#creaste required folders, copy files to correct location, make scripts executable
 cd ./files
 mkdir /usr/share/services/
 mkdir /var/log/manjaro-automatic-update/
@@ -17,5 +19,7 @@ systemctl enable update_shutdown.service
 systemctl start update_shutdown.service
 chmod +x /usr/share/services/shutdown_update
 chmod +x /usr/share/services/startup_update
+#fix the font of plymouth not being visible of the manjaro theme
 sed -i 's/_//' /usr/share/plymouth/themes/manjaro/manjaro.plymouth
+#set the manjaro theme as default
 plymouth-set-default-theme -R manjaro

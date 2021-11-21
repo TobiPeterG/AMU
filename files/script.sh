82cb765802d10932b0cf95aee885bb8a29c66043
Tobi_Peter <19935382+TobiPeterG@users.noreply.github.com>
2021-11-21 16:47:41 +0100

    correct initial commit
    actually don't be dumb

--- files/script.sh
+++ files/script.sh
@@ -5,7 +5,7 @@ read _
 
 cd ./files
 mkdir /usr/share/services/
-mkdir /var/log/ubuntu-automatic-update/
+mkdir /var/log/manjaro-automatic-update/
 cp ./shutdown_update /usr/share/services/shutdown_update
 cp ./startup_update /usr/share/services/startup_update
 cp ./update_shutdown.service /etc/systemd/system/update_shutdown.service
@@ -15,17 +15,6 @@ systemctl daemon-reload
 systemctl enable update_startup.service
 systemctl enable update_shutdown.service
 systemctl start update_shutdown.service
-apt-get install expect -y
-apt-get install --reinstall plymouth-theme-spinner -y
-update-alternatives --set default.plymouth /usr/share/plymouth/themes/bgrt/bgrt.plymouth
-if [ -f /usr/share/plymouth/themes/kubuntu-logo/images/logo.png ];
-then
-  cp /usr/share/plymouth/themes/spinner/watermark.png /usr/share/plymouth/themes/spinner/watermark-old.png
-  cp /usr/share/plymouth/themes/kubuntu-logo/images/logo.png /usr/share/plymouth/themes/spinner/watermark.png
-  cp /usr/share/plymouth/ubuntu-logo.png /usr/share/plymouth/ubuntu-logo_old.png
-  cp /usr/share/plymouth/themes/kubuntu-logo/images/logo.png /usr/share/plymouth/ubuntu-logo.png
-fi
 chmod +x /usr/share/services/shutdown_update
 chmod +x /usr/share/services/startup_update
-update-initramfs -c -k all
 
--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 chmod +x ./files/script.sh
-x-terminal-emulator -e sudo ./files/script.sh
+konsole -e sudo ./files/script.sh

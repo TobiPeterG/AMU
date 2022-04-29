915ce5215afb6b895addea132f3aa66fb94d1833
Tobi_Peter <19935382+TobiPeterG@users.noreply.github.com>
2022-04-29 13:59:41 +0200

    auto-install plymouth
    auto install plymouth during installation, if not installed, edit config files and create backups for them

--- files/script.sh
+++ files/script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #show warning and wait for user to hit enter or abort installation
-echo "This script will install my custom script to\nupdate your system when it's being shutdown.\nIt is tested on Manjaro 21.1.6, but should work\nwith future versions as well.\n\nThe script requires root privileges to copy the files to the\ncorresponding locations.\n\nThis script also includes a service that updates your\nflatpak and snap packages at startup."
+echo -e "This script will install my custom script to\nupdate your system when it's being shutdown.\nIt is tested on Manjaro 21.1.6, but should work\nwith future versions as well.\n\nThe script requires root privileges to copy the files to the\ncorresponding locations.\n\nThis script also includes a service that updates your\nflatpak and snap packages at startup."
 printf 'press [ENTER] to continue, [CTRL]+[C] to abort...'
 read _
 
-#creaste required folders, copy files to correct location, make scripts executable
+#create required folders, copy files to correct location, make scripts executable
 cd ./files
 mkdir /usr/share/services/
 mkdir /var/log/manjaro-automatic-update/
@@ -22,6 +22,23 @@ systemctl enable update_shutdown.service
 systemctl start update_shutdown.service
 chmod +x /usr/share/services/shutdown_update
 chmod +x /usr/share/services/startup_update
+
+#install plymouth, if not installed
+if ! command -v /bin/plymouth;
+then
+    pamac install plymouth plymouth-theme-manjaro --no-confirm
+    #backup config files
+    cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf-backup
+    cp /etc/default/grub  /etc/default/grub-backup
+    #add / change required init hooks
+    sed -i 's/HOOKS="base udev/HOOKS="base udev plymouth/' /etc/mkinitcpio.conf
+    sed -i 's/encrypt resume/plymouth-encrypt resume/' /etc/mkinitcpio.conf
+    #change grub config to show plymouth
+    sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash/' /etc/default/grub
+    sudo mkinitcpio -P
+    sudo update-grub
+fi
+
 
 #fix the font of plymouth not being visible of the manjaro theme
 sed -i 's/_//' /usr/share/plymouth/themes/manjaro/manjaro.plymouth

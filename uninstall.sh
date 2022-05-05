00a349f619ae296e6bcbbdb28c7b82e6ae8741f0
Tobi_Peter <19935382+TobiPeterG@users.noreply.github.com>
2022-05-06 00:33:58 +0200

    rename script files (4/4)

--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,8 @@
 #disable installed services and remove all files and folders that were created by the installer
 systemctl disable update_startup.service
 systemctl disable update_shutdown.service
-rm -rf /usr/local/bin/shutdown_update
-rm -rf /usr/local/bin/startup_update
+rm -rf /usr/local/bin/update_shutdown
+rm -rf /usr/local/bin/update_startup
 rm /etc/systemd/system/update_shutdown.service
 rm /etc/systemd/system/update_startup.service
 rm -rf /etc/systemd/system/poweroff.target.d

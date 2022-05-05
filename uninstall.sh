cb1e4268c30753f733fd36c174f3a95cd18001d6
Tobi_Peter <19935382+TobiPeterG@users.noreply.github.com>
2022-05-06 00:27:17 +0200

    update location of scripts in uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,8 @@
 #disable installed services and remove all files and folders that were created by the installer
 systemctl disable update_startup.service
 systemctl disable update_shutdown.service
-rm -rf mkdir /usr/share/services/
+rm -rf /usr/local/bin/shutdown_update
+rm -rf /usr/local/bin/startup_update
 rm /etc/systemd/system/update_shutdown.service
 rm /etc/systemd/system/update_startup.service
 rm -rf /etc/systemd/system/poweroff.target.d
